4a692f7213da43b9ca692be957fe133664648b2c
Sudo-Si <dalgleishmaswabi@gmail.com>
2022-09-01 11:08:36 +1200

    POST Requests working  astra has weird tokens have to paste twice

--- server.js
+++ server.js
@@ -9,36 +9,36 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const url = 'https://9c634c1a-d354-4cc6-9aad-a2042cf8ca7d-asia-south1.apps.astra.datastax.com/api/rest/v2/namespaces/tickets/collections/tasks'
-const token = 'AstraCS:PJCRzPGGOOBqgTTbyRyEMhpz:a6462f39e9fe8b82d29018c6522d65382ecda5ad156a84897e5a93e950a9e0f5'
+const url = 'https://b655984e-71fe-4dfb-986e-8fdee2c42580-asia-south1.apps.astra.datastax.com/api/rest/v2/namespaces/tickets/collections/tasks'
+const token = 'AstraCS:mfmSXFESRFtzujPkYOueRtzo:649ae2536f73e2a76ae70a5f3283c515932cddd7bfb0eeb4e47229436bfbfb51'
 
-app.get('/tickets', async (req, res) => {
-    const options ={
-        method: 'GET', 
-        headers:{
-            Accepts:'application/json',
-            'X-Cassandra-Token': token,
-        }
-    }
-    try {
-        const response = await axios.get(`${url}?page-size=20`, options)
-        res.status(200).json(response.data)
-    } catch (err) {
-        console.log(err)
-        .res.status(500).json({message: err})
+// app.get('/tickets', async (req, res) => {
+//     const options ={
+//         method: 'GET', 
+//         headers:{
+//             Accepts:'application/json',
+//             'X-Cassandra-Token': token,
+//         }
+//     }
+//     try {
+//         const response = await axios.get(`${url}?page-size=20`, options)
+//         res.status(200).json(response.data)
+//     } catch (err) {
+//         console.log(err)
+//         .res.status(500).json({message: err})
         
-    }
-})
+//     }
+// })
 
 app.post('/tickets', async(req, res)=>{
     const formData = req.body.formData
 
-    const options ={
-        method: 'POST',
+    const options = {
+        method : 'POST',
         headers:{
-            Accepts:'application/json',
+            Accepts :'application/json',
             'X-Cassandra-Token': token,
-            'Content-Type' :' application/json'
+            'Content-Type' : 'application/json'
         },
         data: formData
     }
---
+++ src/context.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+const CategoriesContext = createContext({
+    categories : null,
+setCategories: ()=>{}
+})
+export default CategoriesContext
